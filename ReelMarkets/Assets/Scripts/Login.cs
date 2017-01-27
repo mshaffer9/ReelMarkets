27f8f0df39c31e4e9770b0ab4259ed01e359e05f
Vagdevi Kondeti <vkondeti@gatech.edu>
2017-01-26 19:36:46 -0500

    Logic for Login

---
+++ ReelMarkets/Assets/Scripts/Login.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+using UserItems;
+using LoadScene;
+
+public class Login : MonoBehaviour {
+    public static User currentUser; 
+    private string inputUsername;
+    private string inputPassword;
+    public InputField username;
+    public InputField password;
+    public UserManager usermanager = new UserManager();
+
+	// Use this for initialization
+	void Start () {
+		
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        inputUsername = username.text;
+        inputPassword = password.text;
+    }
+
+    public void LoginButton()
+    {
+        bool isValid = (inputUsername != null && inputPassword != null 
+            && !inputUsername.Equals("") && !inputPassword.Equals(""));
+        if (isValid && usermanager.isExistingUser(inputUsername))
+        {
+            User userInfo = usermanager.getUser(inputUsername);
+            if (userInfo != null && userInfo.Password.Equals(inputPassword)) {
+                currentUser = userInfo;
+                //LoadSceneOnClick.LoadByIndex(2);
+                Debug.Log("Login worked"); 
+            } else
+            {
+                Debug.Log("Invalid login"); //wrong password
+            }
+        } else
+        {
+            Debug.Log("Invalid login"); //fields blank or username does not exist
+        }
+        // else user doesn't exist
+    }
+
+}
---
+++ ReelMarkets/Assets/Scripts/Objects/UserManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UserItems
+{
+    public class UserManager
+    {
+
+        Dictionary<string, User> map = new Dictionary<string, User>()
+    {
+            {"bob", new User("bob", "password", "bob@waters.com", "09-05-1922", User.Gender.MALE, User.AccountType.MODERATOR) },
+            { "admin", new User("admin", "admin", "admin@admin.com", "01-01-1999", User.Gender.MALE, User.AccountType.ADMIN) },
+            { "resercher", new User("researcher", "science", "research@gatech.edu", "04-12-1993", User.Gender.FEMALE, User.AccountType.USER) }
+    };
+
+        public bool isExistingUser(string name)
+        {
+            return map.ContainsKey(name.ToLower());
+        }
+
+        public void addNewUser(User user)
+        {
+            map[user.Username.ToLower()] = user;
+        }
+
+        public void removeUser(string user)
+        {
+            map.Remove(user.ToLower());
+        }
+
+        public void removeUser(User user)
+        {
+            removeUser(user.Username);
+        }
+
+        public User getUser(string username)
+        {
+            if (isExistingUser(username.ToLower()))
+            {
+                return map[username];
+            } else
+            {
+                return null;
+            }
+        }
+    }
+}
