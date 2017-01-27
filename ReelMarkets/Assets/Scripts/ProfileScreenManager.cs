45f7dabdb407dd406bcfac96399c29bf3e88e5af
Marcelles L'von Lowery <marcelleslowery@gmail.com>
2017-01-27 17:59:30 -0500

    Adding ScreenManagerScript
    Adding script

---
+++ ReelMarkets/Assets/Scripts/ProfileScreenManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ProfileScreenManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void onLogout() {
+		//TODO: end user session
+
+		SceneManager.LoadScene (0);
+	}
+
+	public void Delete() {
+		//TODO: delete user from map
+
+
+		SceneManager.LoadScene (0);
+	}
+}
